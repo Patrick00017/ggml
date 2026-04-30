77e8cc94d2bfd0d1ba8c3baf896651b3e38584ee
patrick <742679653@qq.com>
2026-04-30 16:15:13 +0800

    example: build cgraph

--- examples/image_classification/image_classification.cpp
+++ examples/image_classification/image_classification.cpp
@@ -434,6 +434,113 @@ bool icls_model_init_from_file(const std::string &fname, icls_model &model){
     return true;
 }
 
+static ggml_tensor* apply_conv2d(ggml_context* ctx, ggml_tensor* input, const conv2d_layer& layer)
+{
+    struct ggml_tensor* result = ggml_conv_2d(ctx, layer.weights, input, layer.stride, layer.stride, layer.padding, layer.padding, 1, 1);
+    if (layer.batch_normalize) {
+        result = ggml_sub(ctx, result, ggml_repeat(ctx, layer.running_mean, result));
+        result = ggml_div(ctx, result, ggml_sqrt(ctx, ggml_repeat(ctx, layer.running_var, result)));
+        result = ggml_mul(ctx, result, ggml_repeat(ctx, layer.bn_weight, result));
+    }
+    result = ggml_add(ctx, result, ggml_repeat(ctx, layer.bias, result));
+    if (layer.activate) {
+        result = ggml_relu(ctx, result);
+    }
+    return result;
+}
+
+static ggml_tensor* apply_block(ggml_context* ctx, ggml_tensor* input, const std::vector<conv2d_layer>& layers, bool is_downsample) {
+    struct ggml_tensor* result = input; // just assign input to result, because the conv block is not inplace.
+    if (is_downsample) {
+        for (int i = 0; i < layers.size() - 1; i++) {
+            result = apply_conv2d(ctx, result, layers[i]);
+        }
+        // handle downsample layer
+        struct ggml_tensor* residual_result = apply_conv2d(ctx, input, layers[layers.size() - 1]);
+        result = ggml_add(ctx, result, residual_result);
+    }
+    else {
+        for (int i = 0; i < layers.size(); i++) {
+            result = apply_conv2d(ctx, result, layers[i]);
+        }
+        result = ggml_add(ctx, result, input);
+    }
+    return result;
+}
+
+static struct ggml_cgraph* build_graph(struct ggml_context* ctx_cgraph, const icls_model& model) {
+    struct ggml_cgraph* gf = ggml_new_graph(ctx_cgraph);
+    struct ggml_tensor* input = ggml_new_tensor_4d(ctx_cgraph, GGML_TYPE_F32, model.input_width, model.input_height, 3, 1);
+    ggml_set_name(input, "input");
+    struct ggml_tensor* result = apply_conv2d(ctx_cgraph, input, model.backbone.conv1);
+    print_shape(0, result);
+    result = ggml_pool_2d(ctx_cgraph, result, GGML_OP_POOL_MAX, 3, 3, 2, 2, 1, 1);
+    print_shape(1, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer1_0, true);
+    print_shape(10, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer1_1, false);
+    print_shape(11, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer1_2, false);
+    print_shape(12, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer2_0, true);
+    print_shape(20, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer2_1, false);
+    print_shape(21, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer2_2, false);
+    print_shape(22, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer2_3, false);
+    print_shape(23, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer3_0, true);
+    print_shape(30, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer3_1, false);
+    print_shape(31, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer3_2, false);
+    print_shape(32, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer3_3, false);
+    print_shape(33, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer3_4, false);
+    print_shape(34, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer3_5, false);
+    print_shape(35, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer4_0, true);
+    print_shape(40, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer4_1, false);
+    print_shape(41, result);
+    result = apply_block(ctx_cgraph, result, model.backbone.layer4_2, false);
+    print_shape(42, result);
+    result = ggml_pool_2d(ctx_cgraph, result, GGML_OP_POOL_AVG, 3, 3, 2, 2, 1, 1); // todo: adaptive avgpool
+    print_shape(50, result);
+    result = ggml_add(ctx_cgraph, ggml_mul_mat(ctx_cgraph, model.backbone.fc_weight, result), model.backbone.fc_bias);
+    print_shape(51, result);
+
+    // start classifer
+    result = ggml_relu(ctx_cgraph, ggml_add(ctx_cgraph, ggml_mul_mat(ctx_cgraph, model.backbone.classifer.fc1_weight, result), model.backbone.classifer.fc1_bias));
+    print_shape(60, result);
+    result = ggml_relu(ctx_cgraph, ggml_add(ctx_cgraph, ggml_mul_mat(ctx_cgraph, model.backbone.classifer.fc2_weight, result), model.backbone.classifer.fc2_bias));
+    print_shape(61, result);
+    result = ggml_relu(ctx_cgraph, ggml_add(ctx_cgraph, ggml_mul_mat(ctx_cgraph, model.backbone.classifer.fc3_weight, result), model.backbone.classifer.fc3_bias));
+    print_shape(62, result);
+    result = ggml_soft_max(ctx_cgraph, result);
+
+    ggml_set_output(result);
+    ggml_set_name(result, "output");
+
+    ggml_build_forward_expand(gf, result);
+
+    return gf;
+}
+
+void inference(icls_image& image, struct ggml_cgraph * gf, const icls_model& model) {
+    icls_image sized = letterbox_image(image, model.input_width, model.input_height);
+    struct ggml_tensor* input = ggml_graph_get_tensor(gf, "input");
+    ggml_backend_tensor_set(input, sized.data.data(), 0, ggml_nbytes(input));
+
+    if (ggml_backend_graph_compute(model.backend, gf) != GGML_STATUS_SUCCESS) {
+        printf("%s: ggml_backend_graph_compute() failed\n", __func__);
+        return;
+    }
+}
+
 int main(int argc, char ** argv){
     ggml_backend_load_all(); // important init
     ggml_time_init(); // important init
@@ -451,4 +558,24 @@ int main(int argc, char ** argv){
 
     // load model
     icls_model_init_from_file(params.model, model);
+
+    // build compute graph
+    struct ggml_init_params params0 = {
+        /*.mem_size   =*/ ggml_tensor_overhead() * GGML_DEFAULT_GRAPH_SIZE + ggml_graph_overhead(),
+        /*.mem_buffer =*/ NULL,
+        /*.no_alloc   =*/ true, // the tensors will be allocated later by ggml_gallocr_alloc_graph()
+    };
+    struct ggml_context* ctx_cgraph = ggml_init(params0);
+    struct ggml_cgraph* gf = build_graph(ctx_cgraph, model);
+    ggml_gallocr_t allocr = ggml_gallocr_new(ggml_backend_get_default_buffer_type(model.backend));
+    ggml_gallocr_alloc_graph(allocr, gf);
+
+    // todo: load input image
+    icls_image image;
+    if (!load_image(params.fname_inp.c_str(), image)) {
+        printf("%s: failed to load image from '%s'\n", __func__, params.fname_inp.c_str());
+        return 1;
+    }
+    // inference
+    inference(image, gf, model);
 }
